22d6df6014dad1a789c6a32f669812bf77fb0217
Terry Liu <terry.liu@kellogg.northwestern.edu>
2023-04-07 21:09:04 +0000

    Lab 4-3

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,5 +1,12 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
+SELECT teams.name, players.first_name, players.last_name, MAX(stats.home_runs)
 
+FROM teams
+INNER JOIN stats on teams.id = stats.team_id
+INNER JOIN players on stats.player_id = players.id
+
+WHERE teams.year = 2019
+;
 -- Expected result:
 --
 -- +---------------+------------+-----------+-----------+
