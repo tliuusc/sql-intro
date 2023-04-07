08e212ac7ed5ccdb58434b26e735f6c5a8701991
Terry Liu <terry.liu@kellogg.northwestern.edu>
2023-04-07 19:00:27 +0000

    Lab 1

--- lab-1-1.sql
+++ lab-1-1.sql
@@ -1,7 +1,9 @@
 -- The teams that play professional baseball can change every year 
 -- (e.g. expansion teams, teams moving, renaming, etc). List the year 
 -- and team name of every baseball team that has ever played.
-
+SELECT year, name
+FROM teams
+;
 -- Expected result: 2955 rows, ending with
 --
 -- | 2020 | Seattle Mariners                  |
