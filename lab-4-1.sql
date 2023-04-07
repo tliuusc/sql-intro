882299e10c5b3cf9409a9b43b4bc60ed1e0b706c
Terry Liu <terry.liu@kellogg.northwestern.edu>
2023-04-07 20:57:06 +0000

    Lab 4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,8 @@
 -- How many lifetime hits does Barry Bonds have?
-
+SELECT SUM(stats.hits)
+FROM players INNER JOIN stats ON players.id = stats.player_id
+WHERE players.id = 1678
+;
 -- Expected result:
 -- 2935
 
